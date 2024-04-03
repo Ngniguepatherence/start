dbf60ab0cf7eeadcff5960d3f27835f052f06720
Ngniguepa Therence <111742624+Ngniguepatherence@users.noreply.github.com>
2024-04-03 02:53:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,62 @@
-from typing import Optional
-
-from fastapi import FastAPI
+import cv2
+import numpy as np
+from fastapi import FastAPI, UploadFile, File
+from PIL import Image
+import uvicorn
+import easyocr
+from typing import List
+from pydantic import BaseModel, AnyHttpUrl
 
 app = FastAPI()
 
+reader = easyocr.Reader(['fr','en'])
 
+# function to convert the image into grayscale
+def convert_to_gray(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return gray
+
+# function to detect edges
+def detect_edges(img):
+    edges = cv2.Canny(img, 100, 200)
+    return edges
+
+# function to extract data using EasyOCR
+def extract_data(img):
+    result = reader.readtext(img)
+    extracted_text = []
+    for (bbox, text, prob) in result:
+        extracted_text.append({'text': text, 'probability': prob})
+    return extracted_text
+
+# function to extract license plate numbers
+
+class ProcessedImage(BaseModel):
+    extracted_data: List
+
+# Define FastAPI endpoints
+@app.post("/process_image/")
+async def process_image(file: UploadFile = File(...)):
+    contents = await file.read()
+    nparr = np.frombuffer(contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    # Process the imag
+    extracted_data = extract_data(img)
+
+    processed_image = ProcessedImage(
+        extracted_data=extracted_data,
+    )
+
+    return processed_image
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def test_api():
+    return {"message": "API is running successfully."}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+
+
+HOST = '0.0.0.0'
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host=HOST, port=8000, reload=True)
